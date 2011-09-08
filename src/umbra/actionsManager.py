eabc9c7eb558d92df2f381fdd0b54859d6aa48a1
KelSolaar <thomas.mansencal@gmail.com>
2011-09-08 23:42:07 +0100

    Add "actionsManager" module.

---
+++ src/umbra/actionsManager.py
@@ -0,0 +1,444 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+**actionsManager.py**
+
+**Platform:**
+	Windows, Linux, Mac Os X.
+
+**Description:**
+	This module defines the :class:`ActionsManager` class.
+
+**Others:**
+
+"""
+
+#***********************************************************************************************
+#***	External imports.
+#***********************************************************************************************
+import logging
+import re
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+
+#***********************************************************************************************
+#***	Internal imports.
+#***********************************************************************************************
+import foundations.core as core
+import foundations.exceptions
+import foundations.namespace
+import foundations.walkers
+import umbra.exceptions
+from umbra.globals.constants import Constants
+
+#***********************************************************************************************
+#***	Module attributes.
+#***********************************************************************************************
+__author__ = "Thomas Mansencal"
+__copyright__ = "Copyright (C) 2008 - 2011 - Thomas Mansencal"
+__license__ = "GPL V3.0 - http://www.gnu.org/licenses/"
+__maintainer__ = "Thomas Mansencal"
+__email__ = "thomas.mansencal@gmail.com"
+__status__ = "Production"
+
+__all__ = ["LOGGER", "ActionsManager"]
+
+LOGGER = logging.getLogger(Constants.logger)
+
+#***********************************************************************************************
+#***	Module classes and definitions.
+#***********************************************************************************************
+class ActionsManager(QObject):
+	"""
+	This class is a `QObject <http://doc.qt.nokia.com/4.7/qobject.html>`_ subclass providing an actions manager.
+	"""
+
+	@core.executionTrace
+	def __init__(self, namespaceSplitter="|", rootNamespace="Actions", defaultNamespace="Others", parent=None):
+		"""
+		This method initializes the class.
+
+		:param namespaceSplitter: Namespace splitters character. ( String )
+		:param rootNamespace: Root namespace. ( String )
+		:param defaultNamespace: Default namespace ( For actions with relative path ). ( String )
+		:param parent: Object parent. ( QObject )
+		"""
+
+		LOGGER.debug("> Initializing '{0}()' class.".format(self.__class__.__name__))
+
+		QObject.__init__(self, parent)
+
+		# --- Setting class attributes. ---
+		self.__namespaceSplitter = None
+		self.namespaceSplitter = namespaceSplitter
+		self.__rootNamespace = None
+		self.rootNamespace = rootNamespace
+		self.__defaultNamespace = None
+		self.defaultNamespace = defaultNamespace
+
+		self.__categories = {}
+
+		self.__actionsSignalsSlots = {}
+
+	#***********************************************************************************************
+	#***	Attributes properties.
+	#***********************************************************************************************
+	@property
+	def namespaceSplitter(self):
+		"""
+		This method is the property for **self.__namespaceSplitter** attribute.
+
+		:return: self.__namespaceSplitter. ( String )
+		"""
+
+		return self.__namespaceSplitter
+
+	@namespaceSplitter.setter
+	@foundations.exceptions.exceptionsHandler(None, False, AssertionError)
+	def namespaceSplitter(self, value):
+		"""
+		This method is the setter method for **self.__namespaceSplitter** attribute.
+
+		:param value: Attribute value. ( String )
+		"""
+
+		if value:
+			assert type(value) in (str, unicode), "'{0}' attribute: '{1}' type is not 'str' or 'unicode'!".format("namespaceSplitter", value)
+			assert len(value) == 1, "'{0}' attribute: '{1}' has multiples characters!".format("namespaceSplitter", value)
+			assert not re.search("\w", value), "'{0}' attribute: '{1}' is an alphanumeric character!".format("namespaceSplitter", value)
+		self.__namespaceSplitter = value
+
+	@namespaceSplitter.deleter
+	@foundations.exceptions.exceptionsHandler(None, False, foundations.exceptions.ProgrammingError)
+	def namespaceSplitter(self):
+		"""
+		This method is the deleter method for **self.__namespaceSplitter** attribute.
+		"""
+
+		raise foundations.exceptions.ProgrammingError("'{0}' attribute is not deletable!".format("namespaceSplitter"))
+
+	@property
+	def rootNamespace(self):
+		"""
+		This method is the property for **self.__rootNamespace** attribute.
+
+		:return: self.__rootNamespace. ( String )
+		"""
+
+		return self.__rootNamespace
+
+	@rootNamespace.setter
+	@foundations.exceptions.exceptionsHandler(None, False, AssertionError)
+	def rootNamespace(self, value):
+		"""
+		This method is the setter method for **self.__rootNamespace** attribute.
+
+		:param value: Attribute value. ( String )
+		"""
+
+		if value:
+			assert type(value) in (str, unicode), "'{0}' attribute: '{1}' type is not 'str' or 'unicode'!".format("rootNamespace", value)
+		self.__rootNamespace = value
+
+	@rootNamespace.deleter
+	@foundations.exceptions.exceptionsHandler(None, False, foundations.exceptions.ProgrammingError)
+	def rootNamespace(self):
+		"""
+		This method is the deleter method for **self.__rootNamespace** attribute.
+		"""
+
+		raise foundations.exceptions.ProgrammingError("'{0}' attribute is not deletable!".format("rootNamespace"))
+
+	@property
+	def defaultNamespace(self):
+		"""
+		This method is the property for **self.__defaultNamespace** attribute.
+
+		:return: self.__defaultNamespace. ( String )
+		"""
+
+		return self.__defaultNamespace
+
+	@defaultNamespace.setter
+	@foundations.exceptions.exceptionsHandler(None, False, AssertionError)
+	def defaultNamespace(self, value):
+		"""
+		This method is the setter method for **self.__defaultNamespace** attribute.
+
+		:param value: Attribute value. ( String )
+		"""
+
+		if value:
+			assert type(value) in (str, unicode), "'{0}' attribute: '{1}' type is not 'str' or 'unicode'!".format("defaultNamespace", value)
+		self.__defaultNamespace = value
+
+	@defaultNamespace.deleter
+	@foundations.exceptions.exceptionsHandler(None, False, foundations.exceptions.ProgrammingError)
+	def defaultNamespace(self):
+		"""
+		This method is the deleter method for **self.__defaultNamespace** attribute.
+		"""
+
+		raise foundations.exceptions.ProgrammingError("'{0}' attribute is not deletable!".format("defaultNamespace"))
+
+	@property
+	def categories(self):
+		"""
+		This method is the property for **self.__categories** attribute.
+
+		:return: self.__categories. ( Dictionary )
+		"""
+
+		return self.__categories
+
+	@categories.setter
+	@foundations.exceptions.exceptionsHandler(None, False, AssertionError)
+	def categories(self, value):
+		"""
+		This method is the setter method for **self.__categories** attribute.
+
+		:param value: Attribute value. ( Dictionary )
+		"""
+
+		if value:
+			assert type(value) is dict, "'{0}' attribute: '{1}' type is not 'dict'!".format("categories", value)
+		self.__categories = value
+
+	@categories.deleter
+	@foundations.exceptions.exceptionsHandler(None, False, foundations.exceptions.ProgrammingError)
+	def categories(self):
+		"""
+		This method is the deleter method for **self.__categories** attribute.
+		"""
+
+		raise foundations.exceptions.ProgrammingError("'{0}' attribute is not deletable!".format("categories"))
+
+	#***********************************************************************************************
+	#***	Class methods.
+	#***********************************************************************************************
+	@core.executionTrace
+	@foundations.exceptions.exceptionsHandler(None, False, Exception)
+	def __normalizeName(self, name):
+		"""
+		This method normalizes provided action name.
+
+		:param name: Action name. ( String )
+		:return: Normalized name. ( Boolean )
+		"""
+
+		if not name.startswith(self.__rootNamespace):
+			name = foundations.namespace.setNamespace(self.__rootNamespace, foundations.namespace.setNamespace(self.__defaultNamespace, name))
+			LOGGER.debug("> Normalized name: '{0}'.".format(name))
+			return name
+		else:
+			LOGGER.debug("> Name '{0}' is already normalized!".format(name))
+			return name
+
+	@foundations.exceptions.exceptionsHandler(None, False, KeyError)
+	def __getCategorie(self, categorie, name, vivify=False):
+		"""
+		This method gets recusively requested categorie, alternately if **vivify** argument is set, the categorie will be created.
+
+		:param categorie: Base categorie. ( Dictionary )
+		:param name: Categorie to retrieve or vivify. ( String )
+		:param vivify: Vivify missing parents in the chain to the requested categorie. ( Boolean )
+		:return: Requested categorie. ( Dictionary )
+		"""
+
+		namespace = foundations.namespace.getNamespace(name, rootOnly=True)
+		name = foundations.namespace.removeNamespace(name, rootOnly=True)
+		if namespace:
+			if vivify and namespace not in categorie.keys():
+				categorie[namespace] = {}
+			return self.__getCategorie(categorie[namespace], name, vivify)
+		else:
+			if vivify and name not in categorie.keys():
+				categorie[name] = {}
+			return categorie[name]
+
+	@core.executionTrace
+	@foundations.exceptions.exceptionsHandler(None, False, umbra.exceptions.CategorieExistsError)
+	def getCategorie(self, name, vivify=False):
+		"""
+		This method returns requested categorie.
+
+		:param name: Categorie to retrieve. ( String )
+		:param vivify: Vivify missing parents in the chain to the requested categorie. ( Boolean )
+		:return: Categorie. ( Dictionary )
+		"""
+
+		categorie = self.__getCategorie(self.__categories, name, vivify)
+		if isinstance(categorie, dict):
+			LOGGER.debug("> Categorie '{0}': '{1}'.".format(name, categorie))
+			return categorie
+		else:
+			raise umbra.exceptions.CategorieExistsError("'{0}' categorie doesn't exists!".format(name))
+
+	@core.executionTrace
+	@foundations.exceptions.exceptionsHandler(None, False, Exception)
+	def addToCategorie(self, categorie, name, action):
+		"""
+		This method adds provided action to given categorie.
+
+		:param categorie: Categorie to store the action. ( String )
+		:param name: Action name. ( String )
+		:param action: Action object. ( QAction )
+		:return: Method success. ( Boolean )
+		"""
+
+		categorie = self.getCategorie(categorie, vivify=True)
+		if not isinstance(categorie, dict):
+			return
+
+		categorie[name] = action
+		LOGGER.debug("> Added '{0}' action to '{1}' categorie!".format(categorie, name))
+		return True
+
+	@core.executionTrace
+	@foundations.exceptions.exceptionsHandler(None, False, Exception)
+	def removeFromCategorie(self, categorie, name):
+		"""
+		This method removes provided action from given categorie.
+
+		:param categorie: Categorie to remove the action from. ( String )
+		:param name: Action name. ( String )
+		:return: Method success. ( Boolean )
+		"""
+
+		categorie = self.getCategorie(categorie)
+		if not isinstance(categorie, dict):
+			return
+
+		del(categorie[name])
+		LOGGER.debug("> Removed '{0}' action from '{1}' categorie!".format(categorie, name))
+		return True
+
+	@core.executionTrace
+	@foundations.exceptions.exceptionsHandler(None, False, umbra.exceptions.ActionExistsError)
+	def getAction(self, name):
+		"""
+		This method returns requested action.
+
+		:param name: Action to get. ( String )
+		:return: Action. ( QAction )
+		"""
+
+		name = self.__normalizeName(name)
+		for path, actionName, action in foundations.walkers.dictionariesWalker(self.__categories):
+			if name == foundations.namespace.setNamespace(self.__namespaceSplitter.join(path), actionName):
+				LOGGER.debug("> Retrieved action for '{0}' action name!".format(name))
+				return action
+		raise umbra.exceptions.ActionExistsError("'{0}' action isn't registered!".format(name))
+
+	@core.executionTrace
+	@foundations.exceptions.exceptionsHandler(None, False, Exception)
+	def registerAction(self, name, **kwargs):
+		"""
+		This method registers provided action name, optional arguments like a parent, icon, slot etc ... can be provided.
+
+		:param name: Action to register. ( String )
+		:param \*\*kwargs: Arguments. ( \* )
+		:return: Action. ( QAction )
+		"""
+
+		settings = core.Structure(**{"parent" : None,
+									"icon" : None,
+									"iconText" : None,
+									"statusTip" : None,
+									"whatsThis" : None,
+									"toolTip" : None,
+									"shortcut" : None,
+									"slot" : None})
+		settings.update(kwargs)
+
+		name = self.__normalizeName(name)
+		categorie = foundations.namespace.getNamespace(name)
+		name = foundations.namespace.removeNamespace(name)
+
+		action = QAction(name, settings.parent or self)
+		self.addToCategorie(categorie, name, action)
+
+		settings.icon and action.setIcon(settings.icon)
+		settings.iconText and action.setIconText(settings.iconText)
+		settings.statusTip and action.setStatusTip(settings.statusTip)
+		settings.whatsThis and action.setWhatsThis(settings.whatsThis)
+		settings.toolTip and action.setToolTip(settings.toolTip)
+		settings.shortcut and action.setShortcut(QKeySequence(settings.shortcut))
+		if settings.slot:
+			self.__actionsSignalsSlots[action] = settings.slot
+			action.triggered.connect(settings.slot)
+		return action
+
+	@core.executionTrace
+	@foundations.exceptions.exceptionsHandler(None, False, Exception)
+	def unregisterAction(self, name):
+		"""
+		This method unregisters provided action name.
+
+		:param name: Action to register. ( String )
+		:return: Method success. ( Boolean )
+		"""
+
+		name = self.__normalizeName(name)
+		action = self.getAction(name)
+		if not action:
+			return
+
+		action.triggered.disconnect(self.__actionsSignalsSlots.pop(action))
+
+		categorie = foundations.namespace.getNamespace(name)
+		name = foundations.namespace.removeNamespace(name)
+		self.removeFromCategorie(categorie, name)
+		return True
+
+	@core.executionTrace
+	@foundations.exceptions.exceptionsHandler(None, False, Exception)
+	def isShortcutInUse(self, shortcut):
+		"""
+		This method returns if provided action shortcut is in use.
+
+		:param name: Action shortcut. ( String )
+		:return: Is shortcut in use. ( Boolean )
+		"""
+
+		for path, actionName, action in foundations.walkers.dictionariesWalker(self.__categories):
+			if action.shortcut() == QKeySequence(shortcut):
+				return True
+		return False
+
+	@core.executionTrace
+	@foundations.exceptions.exceptionsHandler(None, False, Exception)
+	def getShortcut(self, name):
+		"""
+		This method returns provided action shortcut.
+
+		:param name: Action to retrieve the shortcut. ( String )
+		:return: Action shortcut. ( String )
+		"""
+
+		name = self.__normalizeName(name)
+		action = self.getAction(name)
+		if not action:
+			return
+
+		return action.shortcut().toString()
+
+	@core.executionTrace
+	@foundations.exceptions.exceptionsHandler(None, False, Exception)
+	def setShortcut(self, name, shortcut):
+		"""
+		This method sets provided action shortcut.
+
+		:param name: Action to set the shortcut. ( String )
+		:param shortcut: Shortcut to set. ( String )
+		:return: Method success. ( Boolean )
+		"""
+
+		name = self.__normalizeName(name)
+		action = self.getAction(name)
+		if not action:
+			return
+
+		action.setShortcut(QKeySequence(shortcut))
+		return True
