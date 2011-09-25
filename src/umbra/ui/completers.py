9f6f9321a4be5ee284b552d692b41cfd97b5a83f
KelSolaar <thomas.mansencal@gmail.com>
2011-09-25 11:16:43 +0200

    Add "ui.completers.EnglishCompleter" class.

--- src/umbra/ui/completers.py
+++ src/umbra/ui/completers.py
@@ -40,11 +40,12 @@ __maintainer__ = "Thomas Mansencal"
 __email__ = "thomas.mansencal@gmail.com"
 __status__ = "Production"
 
-__all__ = ["LOGGER", "PYTHON_TOKENS_FILE" , "PythonCompleter"]
+__all__ = ["LOGGER", "PYTHON_TOKENS_FILE" , "ENGLISH_WORDS_FILE", "PythonCompleter", "EnglishCompleter"]
 
 LOGGER = logging.getLogger(Constants.logger)
 
 PYTHON_TOKENS_FILE = umbra.ui.common.getResourcePath(UiConstants.pythonTokensFile)
+ENGLISH_WORDS_FILE = umbra.ui.common.getResourcePath("others/English_Words.rc")
 
 #***********************************************************************************************
 #***	Module classes and definitions.
@@ -136,5 +137,96 @@ class PythonCompleter(QCompleter):
 
 		extendedWords = self.__pythonTokens[:]
 		extendedWords.extend((word for word in set(words) if word not in self.__pythonTokens))
+		self.setModel(QStringListModel(extendedWords))
+		return True
+
+class EnglishCompleter(QCompleter):
+	"""
+	This class is a `QCompleter <http://doc.qt.nokia.com/4.7/qcompleter.html>`_ subclass used as an english text completion widget.
+	"""
+
+	@core.executionTrace
+	def __init__(self, parent=None):
+		"""
+		This method initializes the class.
+
+		:param parent: Widget parent. ( QObject )
+		"""
+
+		LOGGER.debug("> Initializing '{0}()' class.".format(self.__class__.__name__))
+
+		# --- Setting class attributes. ---
+		self.__setEnglishWords()
+
+		QCompleter.__init__(self, self.__englishWords, parent)
+
+		self.setCaseSensitivity(Qt.CaseSensitive)
+		self.setCompletionMode(QCompleter.PopupCompletion)
+
+	#***********************************************************************************************
+	#***	Attributes properties.
+	#***********************************************************************************************
+	@property
+	def englishWords(self):
+		"""
+		This method is the property for **self.__englishWords** attribute.
+
+		:return: self.__englishWords. ( Tuple / List )
+		"""
+
+		return self.__englishWords
+
+	@englishWords.setter
+	@foundations.exceptions.exceptionsHandler(None, False, AssertionError)
+	def englishWords(self, value):
+		"""
+		This method is the setter method for **self.__englishWords** attribute.
+
+		:param value: Attribute value. ( Tuple / List )
+		"""
+
+		if value:
+			assert type(value) in (tuple, list), "'{0}' attribute: '{1}' type is not 'tuple' or 'list'!".format("englishWords", value)
+		self.__englishWords = value
+
+	@englishWords.deleter
+	@foundations.exceptions.exceptionsHandler(None, False, foundations.exceptions.ProgrammingError)
+	def englishWords(self):
+		"""
+		This method is the deleter method for **self.__englishWords** attribute.
+		"""
+
+		raise foundations.exceptions.ProgrammingError("'{0}' attribute is not deletable!".format("englishWords"))
+
+	#***********************************************************************************************
+	#***	Class methods.
+	#***********************************************************************************************
+	@core.executionTrace
+	@foundations.exceptions.exceptionsHandler(None, False, Exception)
+	def __setEnglishWords(self):
+		"""
+		This method sets the english words.
+
+		:return: Method success. ( Boolean )
+		"""
+
+		self.__englishWords = []
+		with open(ENGLISH_WORDS_FILE, "r") as file:
+			for line in iter(file):
+				self.__englishWords.append(line.strip())
+		return True
+
+	@core.executionTrace
+	@foundations.exceptions.exceptionsHandler(None, False, Exception)
+	def updateModel(self, words):
+		"""
+		This method updates the completer model.
+
+		:param words: Words to update the completer with. ( Tuple / List )
+		:return: Method success. ( Boolean )
+		"""
+
+		extendedWords = self.__englishWords[:]
+		extendedWords.extend((word for word in set(words) if word not in self.__englishWords))
 		self.setModel(QStringListModel(extendedWords))
 		return True
