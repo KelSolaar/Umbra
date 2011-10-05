5cfc6e2e981798329b0c27ca4b6ad603d8f906a0
KelSolaar <thomas.mansencal@gmail.com>
2011-10-05 21:06:48 +0100

    Remove debug print statement in "ui.inputAccelerators" module.

--- src/umbra/ui/inputAccelerators.py
+++ src/umbra/ui/inputAccelerators.py
@@ -105,7 +105,6 @@ def completionPreEventInputAccelerators(container, event):
 				popup.setCurrentIndex(container.completer.completionModel().index(0, 0))
 
 				completerRectangle = container.cursorRect()
-				print completerRectangle.getCoords()
 				completerRectangle.setWidth(container.completer.popup().sizeHintForColumn(0) + container.completer.popup().verticalScrollBar().sizeHint().width())
 				container.completer.complete(completerRectangle)
 	else:
