3f16c3c66b299c9db6c3c6c565151532f7a18744
KelSolaar <thomas.mansencal@gmail.com>
2012-01-05 17:24:22 +0000

    Raise version number to "1.0.1".

--- src/umbra/globals/constants.py
+++ src/umbra/globals/constants.py
@@ -46,10 +46,10 @@ class Constants():
 	"""Package major version: '**4**' ( String )"""
 	minorVersion = "0"
 	"""Package minor version: '**0**' ( String )"""
-	changeVersion = "0"
+	changeVersion = "1"
 	"""Package change version: '**0**' ( String )"""
 	releaseVersion = ".".join((majorVersion, minorVersion, changeVersion))
-	"""Package release version: '**1.0.0**' ( String )"""
+	"""Package release version: '**1.0.1**' ( String )"""
 
 	logger = "Umbra_Logger"
 	"""Package logger name: '**Umbra_Logger**' ( String )"""
