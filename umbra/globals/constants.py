bbeafe629d9729a4209f50693b48d0c0643941d9
Thomas Mansencal <thomas.mansencal@gmail.com>
2012-11-28 08:24:14 +0000

    Raise package release version number to "1.0.5".

--- umbra/globals/constants.py
+++ umbra/globals/constants.py
@@ -46,10 +46,10 @@ class Constants():
 	"""Package major version: '**4**' ( String )"""
 	minorVersion = "0"
 	"""Package minor version: '**0**' ( String )"""
-	changeVersion = "4"
-	"""Package change version: '**4**' ( String )"""
+	changeVersion = "5"
+	"""Package change version: '**5**' ( String )"""
 	releaseVersion = ".".join((majorVersion, minorVersion, changeVersion))
-	"""Package release version: '**1.0.4**' ( String )"""
+	"""Package release version: '**1.0.5**' ( String )"""
 
 	logger = "Umbra_Logger"
 	"""Package logger name: '**Umbra_Logger**' ( String )"""
