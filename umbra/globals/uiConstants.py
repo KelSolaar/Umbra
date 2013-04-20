18500de2bc0337ac34b633d1b71ad93fef99096b
Thomas Mansencal <thomas.mansencal@gmail.com>
2013-04-20 15:17:52 +0200

    Restore "Crittercism" application id.

--- umbra/globals/uiConstants.py
+++ umbra/globals/uiConstants.py
@@ -132,5 +132,5 @@ class UiConstants():
 	invalidLinkHtmlFile = "htmls/Invalid_Link.html"
 	"""Invalid link html file: '**htmls/Invalid_Link.html**' ( String )"""
 
-	crittercismId = "5075c158d5f9b9796b000002"
+	crittercismId = "51290b63421c983d17000490"
 	"""Crittercism Id: '**51290b63421c983d17000490**' ( String )"""
