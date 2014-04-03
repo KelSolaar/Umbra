4528c532f66108333245a19779f2bace80e13d2c
Thomas Mansencal <thomas.mansencal@gmail.com>
2014-04-03 22:32:25 +0200

    Update "utilities/gatherFiles.sh" file.

--- utilities/gatherFiles.sh
+++ utilities/gatherFiles.sh
@@ -3,23 +3,23 @@ echo ---------------------------------------------------------------------------
 echo Umbra - Files Gathering
 echo -------------------------------------------------------------------------------
 
-export PROJECT=$( dirname "${BASH_SOURCE[0]}" )/..
+export PROJECT_DIRECTORY=$(cd $( dirname "${BASH_SOURCE[0]}" )/..; pwd)
 
-export DOCUMENTATION=$PROJECT/docs/
-export RELEASES=$PROJECT/releases/
-export REPOSITORY=$RELEASES/repository/
-export UTILITIES=$PROJECT/utilities
+export DOCUMENTATION_DIRECTORY=$PROJECT_DIRECTORY/docs/
+export RELEASES_DIRECTORY=$PROJECT_DIRECTORY/releases/
+export REPOSITORY_DIRECTORY=$RELEASES_DIRECTORY/repository/
+export UTILITIES_DIRECTORY=$PROJECT_DIRECTORY/utilities
 
 #! Gathering folder cleanup.
-rm -rf $REPOSITORY
-mkdir -p $REPOSITORY/Umbra
+rm -rf $REPOSITORY_DIRECTORY
+mkdir -p $REPOSITORY_DIRECTORY/Umbra
 
 #! Umbra Changes gathering.
-cp -rf $RELEASES/Changes.html $REPOSITORY/Umbra/
+cp -rf $RELEASES_DIRECTORY/Changes.html $REPOSITORY_DIRECTORY/Umbra/
 
 #! Umbra Manual / Help files.
-cp -rf $DOCUMENTATION/help $REPOSITORY/Umbra/Help
-rm $REPOSITORY/Umbra/help/Umbra_Manual.rst
+cp -rf $DOCUMENTATION_DIRECTORY/help $REPOSITORY_DIRECTORY/Umbra/Help
+rm $REPOSITORY_DIRECTORY/Umbra/help/Umbra_Manual.rst
 
 #! Umbra Api files.
-cp -rf $DOCUMENTATION/sphinx/build/html $REPOSITORY/Umbra/Api
+cp -rf $DOCUMENTATION_DIRECTORY/sphinx/build/html $REPOSITORY_DIRECTORY/Umbra/Api
